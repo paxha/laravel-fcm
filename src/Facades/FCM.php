fc133495dd736ff88ac207b8d1af7c12b799aeb2
Pasha <hassan.raza@manafatech.com>
2024-05-24 12:36:57 +0500

    3.1

--- src/Facades/FCM.php
+++ src/Facades/FCM.php
@@ -4,6 +4,10 @@ namespace LaravelFCM\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void sendMessage($token, $title, $body, $data = null)
+ * */
+
 class FCM extends Facade
 {
     protected static function getFacadeAccessor(): string
